4edc78c35a91f5bca9d0d82e12aba7e203f393e9
MatthewJOwens <mjowens@mail.com>
2020-01-18 11:42:37 -0700

    Initial commit, function finished

---
+++ main.js
@@ -0,0 +1,129 @@
+// BUTTONS
+let startButton = document.getElementById("start-button")
+let inflateButton = document.getElementById("inflate-button")
+
+//#region GAME LOGIC AND DATA
+
+// DATA
+let clickCount = 0
+let balloonHeight = 120
+let balloonWidth = 100
+let inflationRate = 20
+let maxSize = 300
+let highestPopCount = 0
+let currentPopCount = 0
+let gameLength = 5000
+let clockId = 0
+let timeRemaining = 0
+let currentPlayer = {}
+
+function startGame() {
+  console.log("Get poppin'!")
+  startButton.setAttribute("disabled", "true")
+  inflateButton.removeAttribute("disabled")
+  startClock()
+  setTimeout(stopGame, gameLength)
+}
+
+function startClock() {
+  timeRemaining = gameLength
+  drawClock()
+  clockId = setInterval(drawClock, 1000)
+}
+
+function stopClock() {
+  clearInterval(clockId)
+}
+
+function drawClock() {
+  let countdownElem = document.getElementById("countdown")
+  countdownElem.innerText = (timeRemaining / 1000).toString()
+  timeRemaining -= 1000
+}
+
+function inflate() {
+  clickCount++
+  balloonHeight += inflationRate
+  balloonWidth += inflationRate
+
+  if (balloonHeight >= maxSize) {
+    console.log("Pop the balloon")
+    currentPopCount++
+    balloonHeight = 0
+    balloonWidth = 0
+  }
+  draw()
+}
+
+function draw() {
+  let balloonElement = document.getElementById("balloon")
+  let clickCountElem = document.getElementById("click-count")
+  let popCountElem = document.getElementById("pop-count")
+  let highPopCountElem = document.getElementById("high-pop-count")
+
+  balloonElement.style.height = balloonHeight + "px"
+  balloonElement.style.width = balloonWidth + "px"
+
+  clickCountElem.innerText = clickCount.toString()
+  popCountElem.innerText = currentPopCount.toString()
+  highPopCountElem.innerText = currentPlayer.topScore.toString()
+}
+
+function stopGame() {
+  console.log("The game is over.")
+  startButton.removeAttribute("disabled")
+  inflateButton.setAttribute("disabled", "true")
+
+  clickCount = 0
+  balloonHeight = 120
+  balloonWidth = 100
+
+  if (currentPopCount > currentPlayer.topScore) {
+    currentPlayer.topScore = currentPopCount
+    savePlayers()
+  }
+  currentPopCount = 0
+
+  stopClock()
+  draw()
+}
+// #endregion
+
+let players = []
+loadPlayers()
+
+function setPlayer(event) {
+  event.preventDefault()
+  let form = event.target
+
+  let playerName = form.playerName.value
+
+  currentPlayer = players.find(player => player.name == playerName)
+
+  if (!currentPlayer) {
+    currentPlayer = { name: playerName, topScore: 0 }
+    players.push(currentPlayer)
+    savePlayers()
+  }
+
+  form.reset()
+  document.getElementById("game").classList.remove("hidden")
+  form.classList.add("hidden")
+  draw()
+}
+
+function changePlayer() {
+  document.getElementById("player-form").classList.remove("hidden")
+  document.getElementById("game").classList.add("hidden")
+}
+
+function savePlayers() {
+  window.localStorage.setItem("players", JSON.stringify(players))
+}
+
+function loadPlayers() {
+  let playersData = JSON.parse(window.localStorage.getItem("players"))
+  if (playersData) {
+    players = playersData
+  }
+}
